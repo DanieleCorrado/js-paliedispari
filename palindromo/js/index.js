4797fd58aea536bc9d34eb023f6244dda8fbc704
DanieleCorrado <corrado_daniele@virgilio.it>
2023-05-08 15:59:47 +0200

    add comment

--- palindromo/js/index.js
+++ palindromo/js/index.js
@@ -1,7 +1,9 @@
 
-// Richiedo all'untente di inesrire la parola
+// Richiedo all'untente di inesrire una parola
 
 let word = prompt("Inseri la parola da controllare");
+
+// assegno la lunghezza della parola e check per il controllo della stringa
 
 let counter = word.length;
 let check = false;
@@ -10,7 +12,7 @@ let check = false;
 
 isPalindrome(word);
 
-// funzione che effettua controlla se la parola inserita dall'utente è palindorma o meno
+// funzione che controlla se la parola inserita dall'utente è palindorma o meno
 
 function isPalindrome(word) {
   
@@ -26,7 +28,6 @@ function isPalindrome(word) {
 
       check = false;
       i = word.length;
-      
       return console.log("La parola inserita non è palindroma");
     }
   
--- paridispari/js/index.js
+++ paridispari/js/index.js
@@ -1,20 +1,21 @@
 // Chiedo all'utente di scegliere tra pari o dispari
 
 userChoise = prompt("Scegli i numeri pari o quelli dispari?");
-console.log(userChoise);
 
-// Chiedo un umero da 1 a 5 all'utente
+// Chiedo un numero da 1 a 5 all'utente
 
 userNumber = parseInt(prompt("inserisci un valore da 1 a 5"));
-console.log(userNumber);
 
 // Genero un numero casuale da 1 a 5
 
 computerNumber = Math.trunc((Math.random() * 5) + 1);
 
-console.log(computerNumber);
+
+// richiamo la funzione 
 
 evenOrOdd(userNumber, computerNumber);
+
+// funzione che  somma il numero inserito dall'utente e quello generato dal computer, controlla se la somma sia pari o dispari e stabilisce il vincitore
 
 function evenOrOdd(userNumber, computerNumber) {
 
