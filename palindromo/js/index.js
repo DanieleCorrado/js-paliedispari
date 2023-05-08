f5292e0cdb0294a3aeaf6a8a81fb19aa60558106
DanieleCorrado <corrado_daniele@virgilio.it>
2023-05-08 15:30:15 +0200

    add js palindrome funcion

--- palindromo/js/index.js
+++ palindromo/js/index.js
@@ -1,1 +1,38 @@
-console.log("prova");
+let check = false;
+
+let word = prompt("Inseri la parola da controllare");
+
+let counter = word.length;
+
+isPalindrome(word);
+
+function isPalindrome(word) {
+  
+  for (let i = 0; i < word.length; i++ ) {
+  
+
+    if(word[i] === word[counter-1]) {
+  
+      check = true;
+      counter--;
+  
+    } else {
+  
+      check = false;
+      i = word.length;
+  
+    }
+  
+  }
+  
+  if (check) {
+  
+    return console.log("La parola inserita è palindroma");
+  
+  } else {
+
+    return console.log("La parola inserita non è palindroma");
+
+  }
+
+}
